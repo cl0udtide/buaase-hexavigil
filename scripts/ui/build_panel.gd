365bdf41d6d4c584eca85841d43d9b1d6f391a35
Z-XUA <531597593@qq.com>
2026-04-24 22:45:57 +0800

    feat(基建)：完成采集类建筑的建造与资源产出逻辑

--- scripts/ui/build_panel.gd
+++ scripts/ui/build_panel.gd
@@ -5,6 +5,9 @@ const AppTheme = preload("res://scripts/ui/app_theme.gd")
 
 func _ready() -> void:
 	AppTheme.apply(self)
+	_bind_button("%BuildLumberButton", &"lumber_station")
+	_bind_button("%BuildStoneButton", &"stone_quarry")
+	_bind_button("%BuildManaButton", &"mana_extractor")
 	_bind_button("%BuildMedicalButton", &"medical_station")
 	_bind_button("%BuildWallButton", &"wood_wall")
 
@@ -21,4 +24,4 @@ func _on_build_button_pressed(building_id: StringName) -> void:
 		action_panel.set_mode_build(building_id)
 	var label := get_node_or_null("%BuildSelectionLabel") as Label
 	if label != null:
-		label.text = "当前建造：%s" % String(building_id)
+		label.text = "Selected: %s" % String(building_id)
